e65df760e580fd60f88bc222c6278f8fe1a8862e
Dan Angelo Alcanar <danangeloalcanar@gmail.com>
2017-11-18 11:23:40 +0800

    Edited node.tpl.php added variables in node preprocessor

--- template.php
+++ template.php
@@ -4,3 +4,12 @@
  * The primary PHP file for this theme.
  */
 
+
+function lilac_preprocess_node(&$variables) {
+
+  // Read More
+  $variables['read_more'] = $variables['content']['links']['node'];
+
+  // Format created date
+  $variables['created_date_formatted'] = date('F d, Y, l h:i A', $variables['created']);
+}
--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -82,6 +82,7 @@
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <?php if ((!$page && !empty($title)) || !empty($title_prefix) || !empty($title_suffix) || $display_submitted): ?>
   <header>
+    <?php print render($read_more); ?>
     <?php print render($title_prefix); ?>
     <?php if (!$page && !empty($title)): ?>
     <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
@@ -90,7 +91,8 @@
     <?php if ($display_submitted): ?>
     <span class="submitted">
       <?php print $user_picture; ?>
-      <?php print $submitted; ?>
+      <?php /* print $submitted; */ ?>
+      <?php print $created_date_formatted; ?>
     </span>
     <?php endif; ?>
   </header>
