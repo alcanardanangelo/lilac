eab421500ecb1c3cdcde74e36e7e68210304975a
Dan Angelo Alacanar <danangeloalcanar@gmail.com>
2017-11-19 23:39:51 -0800

    --

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,10 +1,22 @@
-<div id="page">
+
+<!-- Title -->
+<?php if ($site_name || $site_slogan):?>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <h2><?php print $site_name; ?> | <?php print $site_slogan; ?></h2>
+      </div>
+    </div>
+  </div>
+<?php endif; ?>
+
+<div id="page"> 
   <div id="main-content" class="<?php print $container_class; ?>">
     <div class="row">
       <?php if (!empty($page['content'])): ?>
         <div id="content" class="col-sm-9">
           <?php if (!$is_front): ?>
-            <h2><?php print $title; ?></h2>
+            <h3><?php print $title; ?></h3>
           <?php endif; ?>
           <?php print render($page['content']);?>
         </div>
