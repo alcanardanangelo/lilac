eedde7af972ad6cb73ed0f53b321a98216bd1ad0
Dan Angelo Alacanar <danangeloalcanar@gmail.com>
2017-11-20 00:09:10 -0800

    Added main menu

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,16 +1,63 @@
-
-<!-- Title -->
-<?php if ($site_name || $site_slogan):?>
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-12">
-        <h2><?php print $site_name; ?> | <?php print $site_slogan; ?></h2>
+<div id="page"> 
+  <!-- Title -->
+  <?php if ($site_name || $site_slogan):?>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <h2><?php print $site_name; ?> | <?php print $site_slogan; ?></h2>
+        </div>
       </div>
     </div>
-  </div>
-<?php endif; ?>
+  <?php endif; ?>
 
-<div id="page"> 
+  <!-- Menu -->
+  <nav class="navbar navbar-default">
+    <div class="container">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="col-sm-9">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+
+      <!-- Collapse Navigation Links -->
+      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+        <div class="navbar-collapse collapse" id="navbar-collapse">
+          <nav role="navigation">
+            <?php if (!empty($primary_nav)): ?>
+              <?php print render($primary_nav); ?>
+            <?php endif; ?>
+            <?php if (!empty($secondary_nav)): ?>
+              <?php print render($secondary_nav); ?>
+            <?php endif; ?>
+            <?php if (!empty($page['navigation'])): ?>
+              <?php print render($page['navigation']); ?>
+            <?php endif; ?>
+          </nav>
+        </div>
+      <?php endif; ?>
+
+      </div>
+
+      <div id="search" class="col-sm-3">
+        <form class="navbar-form navbar-right">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Search">
+          </div>
+          <button type="submit" class="btn btn-default">S</button>
+        </form>
+      </div>
+
+      
+    </div><!-- /.container -->
+  </nav>
+
+
+  <!-- Main Content -->
   <div id="main-content" class="<?php print $container_class; ?>">
     <div class="row">
       <?php if (!empty($page['content'])): ?>
